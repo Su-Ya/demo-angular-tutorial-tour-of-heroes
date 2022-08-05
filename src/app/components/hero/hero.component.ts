0ecdc1cd7f81fd33571ea90f36db3488b243b50a
mangoSu <acid7820@gmail.com>
2022-08-05 19:53:52 +0800

    feat: use HeroService to get hero, use ActivatedRoute to get route params heroId

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,15 +1,24 @@
+import { HeroService } from './../../service/hero.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from "../../../types/hero";
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent implements OnInit {
+  hero: Hero | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService
+  ) { }
 
   ngOnInit(): void {
+    const heroId = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(heroId)
+      .subscribe( hero => this.hero = hero);
   }
 
 }
