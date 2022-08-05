d8e2a48979a57fdcdab27b8cf39a703577ef0c03
mangoSu <acid7820@gmail.com>
2022-08-05 14:38:31 +0800

    feat: create base template header、dashboared、heroes、hero on components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroComponent,
   ],
   imports: [
     BrowserModule,
