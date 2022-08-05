05b8c4c79d81bf22fbcb0848dc597d46bf2038f0
mangoSu <acid7820@gmail.com>
2022-08-05 20:40:51 +0800

    feat: use HeroService to get heroes and slice first 5

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,19 @@
+import { HeroService } from './../../service/hero.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from "../../../types/hero";
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  top5Heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.heroService.getHeroes()
+      .subscribe( heroes => this.top5Heroes = heroes.slice(0, 5));
   }
 
 }
