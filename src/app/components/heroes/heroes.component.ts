d58cfe67729558043d26f89b1bcd6aa1e848e6d5
mangoSu <acid7820@gmail.com>
2022-08-05 19:25:34 +0800

    feat: use HeroService to get heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,15 +1,20 @@
+import { HeroService } from './../../service/hero.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from "../../../types/hero";
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
   }
+
 
 }
