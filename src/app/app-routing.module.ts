7466019cc7fbd2b3a76d014c814f6268871642a4
mangoSu <acid7820@gmail.com>
2022-08-05 14:34:13 +0800

    feat: add base route: root、home、404、**, root route redirect to home, ** route redirect to 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '404',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
