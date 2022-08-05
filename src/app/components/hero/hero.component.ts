f8e6735d45648025c1a5a55d0575e36d8e2849f1
mangoSu <acid7820@gmail.com>
2022-08-05 20:12:46 +0800

    feat: use Location to back previous page, add back button、scss

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -2,6 +2,7 @@ import { HeroService } from './../../service/hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../../../types/hero";
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
@@ -12,13 +13,18 @@ export class HeroComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private heroService: HeroService
+    private heroService: HeroService,
+    private location: Location
   ) { }
 
   ngOnInit(): void {
     const heroId = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(heroId)
       .subscribe( hero => this.hero = hero);
+  }
+
+  back(): void {
+    this.location.back();
   }
 
 }
